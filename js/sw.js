e52123dce225c52c6124771958d4f51800c72662
abdelkader <abdelkaderlaabidi1987@gmail.com>
2020-12-26 12:00:08 +0100

    kjkj

--- js/sw.js
+++ js/sw.js
@@ -1,14 +1,18 @@
-self.addEventListener(type:'install', listener: evt=>{
-
-
-    console.log(evt);
-});
-self.addEventListener('activate', listener:evt =>{
-
-
+self.addEventListener
+(
+    type:'install', listener: evt=>
+{
     console.log(evt);
 }
+);
 
 
 
-    )
+
+self.addEventListener
+(
+    'activate', listener: evt=>
+{
+    console.log(evt);
+}
+)
