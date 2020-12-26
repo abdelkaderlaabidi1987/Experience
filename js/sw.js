9f22247e91c18622abcd2b296ba0a0ca6f51def1
abdelkader <abdelkaderlaabidi1987@gmail.com>
2020-12-26 12:14:37 +0100

    ajouter service woreker2

---
+++ js/sw.js
@@ -0,0 +1,14 @@
+self.addEventListener(type:'install', listener: evt=>{
+
+
+    console.log(evt);
+});
+self.addEventListener('activate', listener:evt =>{
+
+
+    console.log(evt);
+}
+
+
+
+    )
